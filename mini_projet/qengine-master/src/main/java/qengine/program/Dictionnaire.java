906f4272649dbfcb572c08ebc88fd08dd047bfc3
Dorine Fontaine <81218677+DorineFontaine@users.noreply.github.com>
2023-12-02 23:27:23 +0100

    Update Dictionnaire.java
    fin de la partie 1 du projet

--- mini_projet/qengine-master/src/main/java/qengine/program/Dictionnaire.java
+++ mini_projet/qengine-master/src/main/java/qengine/program/Dictionnaire.java
@@ -4,103 +4,65 @@ import java.util.Hashtable;
 import java.util.Map;
 
 
-
+/******************************CLASSE DICTIONNAIRE *************************************/
 
 public class Dictionnaire<K, V> {
 	
 	Dictionary<K,V> dictionnaireEncode;
 	
 	
-	
+	//constructeur 
 	public Dictionnaire() {
-		
 		dictionnaireEncode = new Hashtable<>();
 	}
 	
+	//remplissage du dictionnaire 
 	public void remplissageEncode(K u, V v) {
-		
-		
-		
-		
-		
-		
-			
-			if(!((Hashtable<K,V>) dictionnaireEncode).containsValue(v)) {
+		if(!((Hashtable<K,V>) dictionnaireEncode).containsValue(v)) {
 				dictionnaireEncode.put( u,v);
-			
 		}
-		
 	}
-	
-	public void remplissageDecode(K u, V v) {
-		
-		dictionnaireEncode.put(u,v);
-			}
-	
-	
-	
 	
 	public String toString() {
 		return dictionnaireEncode.toString(); 
-	}
+		}
 	
-	
-	
-	
-	
-	
+	//on renvoie la taille du dictionnaire 
 	public int size() {
-		
-		return dictionnaireEncode.size();
-		
-	}
+		return dictionnaireEncode.size(); 
+		}
 	
+	
+	//affichage du dictionnaire 
 	public void affichage() {
-		
-		
-        for (Map.Entry<K, V> entry : ((Hashtable<K, V>) dictionnaireEncode).entrySet()) {
+		for (Map.Entry<K, V> entry : ((Hashtable<K, V>) dictionnaireEncode).entrySet()) {
             K key = entry.getKey();
             V value = entry.getValue();
             System.out.println(  key + " : " + value);
         }
 	}
 	
-	
+	//On inverse le dictionnaire  Dictionnaire<K, V> --> Dictionnaire<V, K>
 	public Dictionnaire<V, K> invert( ) {
-
 		Dictionnaire<V, K> inv = new Dictionnaire<V, K>();
-		
 		for (Map.Entry<K, V> entry : ((Hashtable<K, V>) dictionnaireEncode).entrySet()) {
             K key = entry.getKey();
             V value = entry.getValue();
             inv.remplissageEncode(value, key);
         }
-
-	    
-	    return inv;
+		return inv;
 	}
 	
-
-	
-	//créer une fonction inverse pour décoder 
-	
-	
-	//marche bien 
-		public V decode(int valeur) {
-			
-			//je rentre la clée pour obtenir la valeur 
-			return dictionnaireEncode.get(valeur);
-			
-		}
+	//On obtient la valeur a partir de la clée
+	//Dictionnaire<K, V>
+	public V decode(int valeur) {
+		return dictionnaireEncode.get(valeur);
+			}
 		
-		
-		//ne marche pas bien 
-		public V encode(String valeur) {
-			
-			
-			return dictionnaireEncode.get(valeur);
-			
-		}
+	//Dictionnaire<V, K>	
+	public V encode(String valeur) {
+		return dictionnaireEncode.get(valeur);
+			}
 		
 	
 	
